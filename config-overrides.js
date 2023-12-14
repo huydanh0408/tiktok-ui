d7e9fb77675a1eca3578e98281f9855470d1ad32
danhhuy <huydanh0408@gmail.com>
2023-12-14 07:49:51 +0700

    Cấu hình Router/Layout cho dự án

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,6 @@
-// Sử dụng cơ chế override của customize-cra
+const { override, useBabelRc } = require("customize-cra");
 
-const { override, useBabelRc } = require("customize-cra"); // commonjs require syntax
-
-// tương tự export default ES6
 module.exports = override(
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  useBabelRc()
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useBabelRc()
 );
