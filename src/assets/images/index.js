68a51eec7d3444c8fa521b697cc5211bd975eaa1
danhhuy <huydanh0408@gmail.com>
2023-12-16 05:15:36 +0700

    UI Header #1: Logo - Search

---
+++ src/assets/images/index.js
@@ -0,0 +1,10 @@
+import searchIcon from './searchIcon';
+import loadingIcon from './loadingIcon';
+
+const images = {
+    logo: require('~/assets/images/logo.svg').default,
+    searchIcon,
+    loadingIcon,
+};
+
+export default images;
--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,13 +1,31 @@
 import className from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 import styles from './Header.module.scss';
+import images from '~/assets/images';
 
 const cx = className.bind(styles);
-console.log('cx:', cx);
-
 function Header() {
     return (
         <div className={cx('wrapper')}>
-            <div className={cx('inner')}>Header</div>
+            <div className={cx('inner')}>
+                <img src={images.logo} alt="Tiktok" />
+                <div className={cx('search')}>
+                    <input
+                        placeholder="Search accounts and videos"
+                        spellCheck={false}
+                    />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <span className={cx('loading')}>{images.loadingIcon}</span>
+
+                    <button className={cx('search-btn')}>
+                        {images.searchIcon}
+                    </button>
+                </div>
+                <div className={cx('action')}>action</div>
+            </div>
         </div>
     );
 }
