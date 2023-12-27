f726bacf719d48d42e233a72b523918f0765bfb5
danhhuy <huydanh0408@gmail.com>
2023-12-27 20:13:17 +0700

    SideBar UI #2.1:

--- src/components/SuggestedAccounts/AccountItem.js
+++ src/components/SuggestedAccounts/AccountItem.js
@@ -3,13 +3,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 
 import styles from './SuggestedAccounts.module.scss';
+import Image from '~/components/Image';
 
 const cx = classNames.bind(styles);
 
 function AccountItem() {
     return (
         <div className={cx('account-item')}>
-            <img
+            <Image
                 className={cx('avatar')}
                 src="https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/f8870ee88db4ac38a8f3324b37dfec32~c5_100x100.jpeg?lk3s=a5d48078&x-expires=1703808000&x-signature=vuGV1b7%2F%2BIMbkV6h5o86EBPavvw%3D"
                 alt=""
